52f90d1cf8392d3f2327bba37d2381959cba23da
CantSayIHave <doug.m.mcld@gmail.com>
2018-10-10 10:48:34 -0600

    rewrote to run only one bot and use new storage manager

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import music_queue
 from response import *
 from util import global_util
 from util.containers import *
+from util.feeds import *
 
 
 class Server(Hashable):
@@ -19,42 +20,28 @@ class Server(Hashable):
 
     """
 
-    __slots__ = ['name', 'mods', 'commands', 'rss', 'command_delay', 'reee_message',
-                 'id', 'reee', 'rolemods', 'spam_timers', 'block_list', 'bot_voice_client',
-                 'search_results', 'suggest_emotes', 'music_player', 'music_chat', 'music_channel',
-                 'music_loading', 'music_timer', 'late', 'current_track', 'vote_skip', 'join_message',
-                 'join_channel', 'leave_channel', 'response_lib', 'music', 'capture', 'birthdays',
-                 'default_role']
+    __slots__ = ['name', 'mods', 'feeds', 'spam_time', 'reee_message',
+                 'id', 'rolemods', 'spam_timers', 'block_list', 'search_results',
+                 'late', 'join_message', 'join_channel', 'leave_channel',
+                 'response_lib', 'music', 'capture', 'birthdays', 'default_role']
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', 'Default')
         self.mods = kwargs.get('mods', [])
-        self.commands = kwargs.get('commands', [])
-        self.rss = kwargs.get('rss', [])
-        self.command_delay = kwargs.get('command_delay', 1)
-        self.reee_message = kwargs.get('reee_msg', 'stop hitting yourself')
+        self.feeds = kwargs.get('feeds', [])  # type: list(RssFeed)
+        self.spam_time = kwargs.get('spam_time', 1)
+        self.reee_message = kwargs.get('reee_message', 'stop hitting yourself')
         self.id = kwargs.get('id', '')
-        self.reee = kwargs.get('reee_state', True)
         self.rolemods = kwargs.get('rolemods', [])
         self.spam_timers = kwargs.get('spam_timers', {})  # custom spam timed commands
         self.block_list = kwargs.get('block_list', [])
-        self.bot_voice_client = None  # actual voice client - for autoplay
         self.search_results = None
-        self.suggest_emotes = []
-        # self.music = deque(kwargs.get('queue', []))
-        self.music_player = None
-        self.music_chat = kwargs.get('music_chat', None)
-        self.music_channel = None  # chat channel player is bound to - for autoplay
-        self.music_loading = False
-        self.music_timer = None  # timer for client disconnect
         self.late = None
-        self.current_track = None
-        self.vote_skip = None
 
         self.default_role = kwargs.get('default_role', None)
 
         # todo: rename rss to feeds (everywhere) and destroy commands
-        # todo: rename command_delay to spam_time
+        # todo: take music_queue instead of queue
 
         # 12/13/17 update
         self.join_message = kwargs.get('join_msg', 'Welcome to the server, @u!')
@@ -62,9 +49,10 @@ class Server(Hashable):
 
         self.leave_channel = kwargs.get('leave_channel', None)
 
-        self.response_lib = ResponseLibrary(self.command_delay)
+        self.response_lib = ResponseLibrary(self.spam_time)
 
-        self.music = music_queue.MusicQueue(queue=kwargs.get('queue', []), bind_chat=kwargs.get('music_chat', None))
+        self.music = music_queue.MusicQueue(queue=kwargs.get('music_queue', []),
+                                            bind_chat=kwargs.get('bind_chat', None))
 
         self.capture = None
 
@@ -91,88 +79,109 @@ class Server(Hashable):
 
         return user.id in self.mods
 
-    def music_timeout(self):
-        """Manages music timeout measurement when called.
-
-        Returns
-        ---------
-        bool
-            an indicator to close the music connection - 'True' or 'False'
-        """
-
-        if self.music_timer is None:
-            self.music_timer = 0
-            return False
-        else:
-            self.music_timer += 10
-
-            if self.music_timer >= global_util.TIME_MUSIC_TIMEOUT:
-                self.music_timer = None
-                return True
-            return False
-
-    def get_rss(self, discord_channel: discord.Channel, uid: str = None, title: str = None, wide_search: bool = False) -> Feed:
-        """Searches for a feed based on uid or title. `wide_search` searches lowercase of both
+    def get_feed(self,
+                 id=None,
+                 discord_channel: discord.Channel = None,
+                 type: str = None,
+                 name: str = None,
+                 channel_id: str = None,
+                 search_lower: bool = False) -> RssFeed:
+        """Searches for a feed based on name. `search_lower` searches lowercase of both
 
         Parameters
-        -----------
-        discord_channel : str
-            id of query discord channel
-        uid : str
-            id of query feed
-        title : str
-            title of query feed
-        wide_search : bool
-            indicates search type. A value of `True` will search both uid and title lowercase
+        ----------
+        id : str
+            the feed absolute id
+        discord_channel : :class:`discord.Channel`
+            query discord channel
+        type : str
+            type of feed
+        name : str
+            handle or username of feed
+        channel_id : str
+            id of query feed channel (if applicable)
+        search_lower : bool
+            a value of `True` will search lowercase
 
         Returns
-        ---------
+        -------
         :class:`Feed`
             The found feed
         """
+        if id:
+            return global_util.iterfind(self.feeds, lambda x: x.id == id)
 
-        if wide_search:
-            if uid:
-                query = uid
-            elif title:
-                query = title
-            else:
-                raise ValueError('Must provide a query.')
-            query = query.lower()
+        if not name or channel_id:
+            raise ValueError('Must provide feed name or channel id.')
 
-            for r in self.rss:  # type: Feed
-                if (r.uid.lower() == query or r.title.lower() == query) and r.channel_id == discord_channel.id:
-                    return r
+        if not discord_channel:
+            raise ValueError('Parameter searches need a discord channel')
+
+        if search_lower:
+            if not name:
+                raise ValueError('Must provide a name to search lowercase.')
+
+            name = name.lower()
+            for feed in self.feeds:  # type: RssFeed
+                if feed.name.lower() == name and feed.discord_channel_id == discord_channel.id:
+                    if type:
+                        if type == feed.string_type():
+                            return feed
+                    else:
+                        return feed
         else:
-            if uid:
-                for r in self.rss:  # type: Feed
-                    if r.uid == uid and r.channel_id == discord_channel.id:
-                        return r
-            elif title:
-                for r in self.rss:  # type: Feed
-                    if r.title == title and r.channel_id == discord_channel.id:
-                        return r
-            else:
-                raise ValueError('Must provide a query.')
+            if name:
+                for feed in self.feeds:  # type: RssFeed
+                    if feed.name == name and feed.discord_channel_id == discord_channel.id:
+                        return feed
+            elif channel_id:
+                for feed in self.feeds:  # type: RssFeed
+                    if feed.channel_id == channel_id and feed.discord_channel_id == discord_channel.id:
+                        return feed
 
-    def add_birthday(self, user: discord.User, dt: datetime):
+    async def add_twitter_feed(self, handle: str):
+        feed = await global_util.olliebot_api.create_twitter_feed(handle)
+        feed.first_time = True
+        self.feeds.append(feed)
+        return feed
+
+    async def add_twitch_feed(self, username):
+        feed = await global_util.olliebot_api.create_twitch_feed(username)
+        feed.first_time = True
+        self.feeds.append(feed)
+        return feed
+
+    async def add_youtube_feed(self, username=None, channel_id=None):
+        feed = await global_util.olliebot_api.create_youtube_feed(username=username, id=channel_id)
+        feed.first_time = True
+        self.feeds.append(feed)
+        return feed
+
+    async def remove_feed(self, feed: RssFeed):
+        deleted = await global_util.olliebot_api.delete_feed(feed)  # server copy
+        self._delete_feed(deleted)  # client copy
+        return deleted
+
+    def add_birthday(self, user: discord.User, dt: datetime) -> Birthday:
         bd = Birthday(user=user, dt=dt)
         self.birthdays.append(bd)
         return bd
 
-    def get_birthdays(self, date=None):
+    def get_birthdays(self, date=None) -> list:
+        """Returns all birthday objects matching a date"""
         if date:
             now = date
         else:
             now = datetime.utcfromtimestamp(time.time() - (4 * 3600))  # UTC - 4 = EST
         b_days = []
-        for b in self.birthdays: # type: Birthday
+        for b in self.birthdays:  # type: Birthday
             if b.dt.day == now.day and b.dt.month == now.month:
                 b_days.append(b)
 
         return b_days
 
-    def get_birthday(self, user=None, date=None):
+    def get_birthday(self, user=None, date=None) -> Birthday:
+        """Returns a single birthday object matching a user or date"""
         if user:
             for b in self.birthdays:  # type: Birthday
                 if user == b.user:
@@ -181,3 +190,9 @@ class Server(Hashable):
             for b in self.birthdays: # type: Birthday
                 if date.day == b.dt.day and date.month == b.dt.month:
                     return b
+
+    # deletes all instances
+    def _delete_feed(self, feed: RssFeed):
+        for f in self.feeds:
+            if f.id == feed.id:
+                self.feeds.remove(feed)
