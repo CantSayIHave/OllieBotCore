aaad1c83804feab9e64fc7f3b24f545f0b99d979
CantSayIHave <doug.m.mcld@gmail.com>
2018-10-10 10:45:36 -0600

    new storage manager using MongoDB

--- storage_manager_v2.py
+++ storage_manager_v2.py
@@ -71,6 +71,7 @@ The 'admins' document in the 'admin' collection is structured like so:
 }
 
 """
+import json
 
 from pymongo import MongoClient
 from pymongo.database import Database
@@ -80,6 +81,7 @@ import discordbot
 from server import Server
 from util import global_util, command_util, exceptions
 from util.containers import *
+from util.feeds import *
 
 """-------------------------------- Mongo Initialization --------------------------------"""
 
@@ -198,7 +200,7 @@ def deserialize(obj: dict, baseclass):
 def update_simple_document(collection: Collection, docname: str, data):
     if collection.find_one(filter={'__docname__': docname}):
         collection.update_one(filter={'__docname__': docname},
-                              update={'data': data})
+                              update={'$set': {'data': data}})
     else:
         collection.insert_one({'__docname__': docname,
                                'data': data})
@@ -207,7 +209,7 @@ def update_simple_document(collection: Collection, docname: str, data):
 def update_document(collection: Collection, docname: str, **data):
     if collection.find_one(filter={'__docname__': docname}):
         collection.update_one(filter={'__docname__': docname},
-                              update=data)
+                              update={'$set': data})
     else:
         data['__docname__'] = docname
         collection.insert_one(data)
@@ -224,6 +226,7 @@ def get_document(collection: Collection, docname: str) -> dict:
 
 
 def get_server_collection(server: Server = None, id: str = None) -> Collection:
+    """Retrieve a server's collection, creates if it does not exist"""
     if id:
         return database['server_{}'.format(id)]
     return database['server_{}'.format(server.id)]
@@ -258,26 +261,30 @@ def load_bot() -> discordbot.DiscordBot:
     server_list = []
 
     for s_id in bot_data['server_ids']:
+
+        s_id = str(s_id)  # reformat all server ids as strings silly
+
         s_collection = get_server_collection(id=s_id)
 
         server_data = get_document(s_collection, docname='server_data')
 
         s_name = server_data['name']
         s_spam_time = server_data['spam_time']
-        s_reee = server_data['reee']
+        s_reee_message = server_data['reee_message']
         s_mods = server_data['mods']
         s_rolemods = server_data['rolemods']
         s_spam_list = server_data['spam_list']
         s_join_msg = server_data['join_msg']
         s_join_channel = server_data['join_channel']
         s_leave_channel = server_data.get('leave_channel', None)
-        s_music_chat = server_data.get('music_chat', None)
+        s_bind_chat = server_data.get('bind_chat', None)
+        s_default_role = server_data.get('default_role', None)
 
         s_block_list = [BlockItem(**command) for command in server_data['block_list']]
 
-        s_feeds = [Feed(**f) for f in get_simple_document(s_collection, docname='feeds')]
+        s_feeds = [build_feed(f) for f in get_simple_document(s_collection, docname='feeds')]
 
-        s_queue = get_simple_document(s_collection, docname='music')
+        s_music_queue = get_simple_document(s_collection, docname='music_queue')
 
         s_responses = get_simple_document(s_collection, docname='responses')
 
@@ -291,25 +298,25 @@ def load_bot() -> discordbot.DiscordBot:
                                   rolemods=s_rolemods,
                                   block_list=s_block_list,
                                   spam_timers=s_spam_list,
-                                  reee_msg=s_reee,
+                                  reee_message=s_reee_message,
                                   join_msg=s_join_msg,
                                   join_channel=s_join_channel,
                                   responses=s_responses,
-                                  queue=s_queue,
-                                  music_chat=s_music_chat,
+                                  music_queue=s_music_queue,
+                                  bind_chat=s_bind_chat,
                                   leave_channel=s_leave_channel,
-                                  birthdays=s_birthdays))  # Server Build
+                                  birthdays=s_birthdays,
+                                  default_role=s_default_role))  # Server Build
 
-    admins = admin.find_one({'__docname__': 'admins'})
+    admins = admin.find_one({'__docname__': 'admins'})['data']
 
-    print('Loaded bot {}'.format(bot_name))
     return discordbot.DiscordBot(name=bot_name,
                                  token=bot_data['token'],
                                  desc=bot_data['desc'],
                                  prefix=bot_data['prefix'],
                                  playing_message=bot_data['playing_message'],
-                                 servers=server_list,
-                                 admins=admins['admins'])
+                                 local_servers=server_list,
+                                 admins=admins)
 
 
 """-------------------------------- Admin Saving --------------------------------
@@ -360,7 +367,7 @@ def write_bot_data(bot: discordbot.DiscordBot):
                 'prefix': bot.command_prefix,
                 'playing_message': bot.playing_message}
 
-    server_ids = [s.id for s in bot.local_servers]
+    server_ids = [str(s.id) for s in bot.local_servers]
 
     bot_dict['server_ids'] = server_ids
 
@@ -384,10 +391,10 @@ def write_bot_data(bot: discordbot.DiscordBot):
         `join_msg`
         `join_channel`
         `leave_channel`
-        `music_chat`
+        `bind_chat`
 
     As well as one simple document for each of the following data:
-        `music`
+        `music_queue`
         `feeds`
         `responses`
         `birthdays`
@@ -406,7 +413,7 @@ def write_bot_data(bot: discordbot.DiscordBot):
 
 
 # target: server.music
-@global_util.global_save
+@global_save
 def write_music(server: Server, s_collection: Collection = None):
     """Writes a server's music queue to database
 
@@ -422,12 +429,12 @@ def write_music(server: Server, s_collection: Collection = None):
         s_collection = get_server_collection(server)
 
     update_simple_document(collection=s_collection,
-                           docname='music',
+                           docname='music_queue',
                            data=server.music.list())
 
 
 # target: server.feeds
-@global_util.global_save
+@global_save
 def write_feeds(server: Server, s_collection: Collection = None):
     """Writes a server's rss feeds to database
 
@@ -444,11 +451,11 @@ def write_feeds(server: Server, s_collection: Collection = None):
 
     update_simple_document(s_collection,
                            docname='feeds',
-                           data=[x.__dict__ for x in server.feeds])
+                           data=[x.as_dict() for x in server.feeds])
 
 
 # target: server.responses
-@global_util.global_save
+@global_save
 def write_responses(server: Server, s_collection: Collection = None):
     """Writes a server's responses to database
 
@@ -469,7 +476,7 @@ def write_responses(server: Server, s_collection: Collection = None):
 
 
 # target: server.birthdays
-@global_util.global_save
+@global_save
 def write_birthdays(server: Server, s_collection: Collection = None):
     """Writes a server's birthdays to database
 
@@ -489,7 +496,7 @@ def write_birthdays(server: Server, s_collection: Collection = None):
                            data=[serialize(x) for x in server.birthdays])
 
 
-@global_util.global_save
+@global_save
 def write_server_data(server: Server, s_collection: Collection = None):
     """Writes a server's "server data" to database
 
@@ -506,7 +513,7 @@ def write_server_data(server: Server, s_collection: Collection = None):
 
     server_dict = {'name': server.name,
                    'spam_time': server.spam_time,
-                   'reee': server.reee_message,
+                   'reee_message': server.reee_message,
                    'mods': server.mods,
                    'rolemods': server.rolemods,
                    'block_list': [x.__dict__ for x in server.block_list],
@@ -514,14 +521,15 @@ def write_server_data(server: Server, s_collection: Collection = None):
                    'join_msg': server.join_message,
                    'join_channel': server.join_channel,
                    'leave_channel': server.leave_channel,
-                   'music_chat': server.music_chat}
+                   'bind_chat': server.music.bind_chat,
+                   'default_role': server.default_role}
 
     update_document(s_collection,
                     docname='server_data',
                     **server_dict)
 
 
-@global_util.global_save
+@global_save
 def write_server(server: Server):
     """Writes a server to the database
 
@@ -548,14 +556,174 @@ def write_server(server: Server):
 
 
 # target: resources.hugs
-@global_util.global_save
+@global_save
 def write_hugs():
     """Writes the hug library to database"""
     update_simple_document(resources, docname='hugs', data=global_util.hug_library)
 
 
 # target: resources.pats
-@global_util.global_save
+@global_save
 def write_pats():
     """Writes the pat library to database"""
     update_simple_document(resources, docname='pats', data=global_util.pat_library)
+
+
+# target: resources.photoshop
+@global_save
+def write_backgrounds(backgrounds: dict):
+    """Writes background list (a bunch of urls)"""
+    update_document(resources, docname='photoshop', backgrounds=backgrounds)
+
+
+# target: resources.stickers
+@global_save
+def write_stickers(stickers: dict):
+    update_simple_document(resources, docname='stickers', data=stickers)
+
+"""-------------------------------- Resources Loading --------------------------------"""
+
+
+# source: resources.hugs
+def load_hugs() -> list:
+    return get_simple_document(resources, docname='hugs')
+
+
+# source: resources.pats
+def load_pats() -> list:
+    return get_simple_document(resources, docname='pats')
+
+
+# source: resources.photoshop
+def load_backgrounds() -> dict:
+    return get_document(resources, docname='photoshop')['backgrounds']
+
+
+# source: resources.stickers
+def load_stickers() -> dict:
+    return get_simple_document(resources, docname='stickers')
+
+
+def ghetto_write_server(path: str, server_name: str):
+    with open('{}/{}/{}.json'.format(path, server_name, server_name), 'r') as file:
+        server_data = json.load(file)
+
+    with open('{}/{}/mods.json'.format(path, server_name), 'r') as file:
+        server_mods = json.load(file)['server_mods']
+
+    s_rss = []
+
+    with open('{}/{}/music.json'.format(path, server_name), 'r') as fi:
+        s_queue = []
+        try:
+            server_queue = json.load(fi)
+            s_queue = server_queue['queue']  # should be a list of dicts
+        except Exception:
+            pass
+
+        if not s_queue:
+            s_queue = []
+
+    with open('{}/{}/responses.json'.format(path, server_name), 'r') as fi:
+        s_responses = json.load(fi)
+
+    s_birthdays = []
+    if global_util.file_exists('{}/{}/birthdays.json'.format(path, server_name)):
+        with open('{}/{}/birthdays.json'.format(path, server_name), 'r') as fi:
+            raw_birthdays = json.load(fi)
+            s_birthdays = [deserialize(x, Birthday) for x in raw_birthdays]
+
+    s_id = server_data['id']
+    s_cmd_delay = server_data['cmd_delay']
+    s_reee = server_data['reee']
+    s_rolemods = server_data['rolemods']
+    s_spam_list = server_data['spam_list']
+    s_block_list = [BlockItem(**command) for command in server_data['block_list']]
+    s_join_msg = server_data['join_msg']
+    s_join_channel = server_data['join_channel']
+    s_leave_channel = server_data.get('leave_channel', None)
+    s_music_chat = server_data.get('music_chat', None)
+    s_default_role = server_data.get('default_role', None)
+
+    s_collection = get_server_collection(id=s_id)
+
+    server_dict = {'name': server_name,
+                   'spam_time': s_cmd_delay,
+                   'reee_message': s_reee,
+                   'mods': server_mods,
+                   'rolemods': s_rolemods,
+                   'block_list': [x.__dict__ for x in s_block_list],
+                   'spam_list': s_spam_list,
+                   'join_msg': s_join_msg,
+                   'join_channel': s_join_channel,
+                   'leave_channel': s_leave_channel,
+                   'bind_chat': s_music_chat,
+                   'default_role': s_default_role}
+
+    update_document(s_collection,
+                    docname='server_data',
+                    **server_dict)
+
+    update_simple_document(collection=s_collection,
+                           docname='music_queue',
+                           data=s_queue)
+
+    update_simple_document(s_collection,
+                           docname='feeds',
+                           data=[x.as_dict() for x in s_rss])
+
+    update_simple_document(s_collection,
+                           docname='responses',
+                           data=s_responses)
+
+    update_simple_document(s_collection,
+                           docname='birthdays',
+                           data=[serialize(x) for x in s_birthdays])
+
+
+def ghetto_write_hugs(filepath: str):
+    """Writes the hug library to database"""
+    with open(filepath, 'r') as file:
+        hug_lib = json.load(file)
+    update_simple_document(resources, docname='hugs', data=hug_lib)
+
+
+# target: resources.pats
+def ghetto_write_pats(filepath: str):
+    """Writes the pat library to database"""
+    with open(filepath, 'r') as file:
+        pat_lib = json.load(file)
+    update_simple_document(resources, docname='pats', data=pat_lib)
+
+
+# target: resources.photoshop
+def ghetto_write_backgrounds(backgrounds: dict):
+    """Writes background list (a bunch of urls)"""
+    update_document(resources, docname='photoshop', backgrounds=backgrounds)
+
+
+# target: resources.stickers
+def ghetto_write_stickers(stickers: dict):
+    update_simple_document(resources, docname='stickers', data=stickers)
+
+
+# target: admin.bot_data
+def ghetto_write_bot_data(token, desc, prefix, playing, ids: list):
+    """Writes bot data to database
+
+    Note: this only writes bot metadata, not server data
+
+    Parameters
+    ----------
+    bot: :class:`discordbot.DiscordBot`
+
+    """
+    bot_dict = {'token': token,
+                'desc': desc,
+                'prefix': prefix,
+                'playing_message': playing,
+                'server_ids': ids}
+
+    update_document(collection=admin,
+                    docname='bot_data',
+                    **bot_dict)
